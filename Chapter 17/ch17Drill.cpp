52f8a2a4cd74f1347c9c9985bdd842649560ea6c
NoName17HUN <38375993+NoName17HUN@users.noreply.github.com>
2021-12-19 15:09:15 +0100

    Updated version_ original upload was with Chapter 18,19,7,8 ... Already had issues

--- Chapter 17/ch17Drill.cpp
+++ Chapter 17/ch17Drill.cpp
@@ -1,12 +1,12 @@
 #include "std_lib_facilities.h"	
-void print_array10(ostream& os, int* a)
+void print_array10(ostream& os, int* arr)
 {
 	for(int i = 0; i < 10; ++i)
 	{
 		os << arr[i] << endl;
 	}
 }
-void print_array(ostream& os, int* a, int n)
+void print_array(ostream& os, int* arr, int n)
 {
 	for(int i = 0; i < n; ++i)
 	{
@@ -21,38 +21,35 @@ void print_vector(ostream& os,vector<int> v, int n)
 	}
 }	
 int main()			
-{	int* a = new int[10];			
+{	int* arr = new int[10];			
 for(int i = 0; i < 10; ++i)
 cout << arr[i] << " " << endl;
-delete[] a;
-cout << endl;	
-cout << "Print array(10)" << endl;	
-int* b = new int[10];
+delete[] arr;
+cout << endl<< "Print array(10)" << endl;	
+int* Barr = new int[10];
 for(int i = 0; i < 10; ++i)
 {
 	Barr[i] = 100 + i;
 }
-print_array10(cout, b);
-delete[] b;
-cout << endl;
-cout << "Print array(11)" << endl;
-int* c = new int[11];
+print_array10(cout, Barr);
+delete[] Barr;
+cout << endl<< "Print array(11)" << endl;
+int* Carr = new int[11];
 for(int i = 0; i < 11; ++i)
 {
 Carr[i] = 100 + i;
 cout << Carr[i] << endl;
 }
-delete[] c;
-cout << endl;
-cout << "Print array(20)" << endl;
-int* d = new int[20];
+delete[] Carr;
+cout << endl<< "Print array(20)" << endl;
+int* Darr = new int[20];
 for(int i = 0; i < 20; ++i)
 {
 Darr[i] = 100 + i;
 }
-print_array(cout, d, 20);
+print_array(cout, Darr, 20);
 		
-delete[] d;
+delete[] Darr;
 	
 cout << endl;
 	
