74419021e772db165d3dffb9f4e4327bcd213769
NoName17HUN <38375993+NoName17HUN@users.noreply.github.com>
2021-12-19 14:56:54 +0100

    Noticed Typos all over file, also solved failed output...

--- Chapter 19/Chapter19.5task.cpp
+++ Chapter 19/Chapter19.5task.cpp
@@ -1,11 +1,11 @@
-#include "../lib_files/std_lib_facilities.h"
+#include "std_lib_facilities.h"
 class Int {
 int value;
 public:
 Int() : value(0) { }                      
 Int(const Int& arg) : value(arg.value) { }  
 Int(int v) : value(v) { }                
-Int& operator=(const Int& v) { value = v.value; }  
+Int& operator=(const Int& v) { value = v.value ;}  
 int get() const { return value; }
 };
 Int operator+(const Int& v1, const Int& v2)
@@ -38,15 +38,15 @@ Int Integer4;
 Integer4 = Integer2;
 Int Integer5 = 6;
 //output with cout
-cout << "Integer1 (using default constructor): " << Integer1 << "\n";
-cout << "Integer2 (using the constructor with arguments): " << Integer2 << "\n";
-cout << "Integer3 (copied constructor from Integer2): " << Integer3 << "\n";
-cout << "Integer4 (copied and assigned  from Integer2): " << Integer4 << "\n";
-cout << "Integer5 (using the constructor with arguments): " << Integer5 << "\n";
-cout << "Integer2 + Integer5 = " << Integer2+Integer5 << "\n";
-cout << "Integer5 - Integer2 = " << Integer5-Integer2 << "\n";
-cout << "Integer2 * Integer5 = " << Integer2*Integer5 << "\n";
-cout << "Integer5 / Integer2 = " << Integer5/Integer2 << "\n";
+cout << "Integer1 (using default constructor): " << Integer1 << endl;
+cout << "Integer2 (using the constructor with arguments): " << Integer2 << endl;
+cout << "Integer3 (copied constructor from Integer2): " << Integer3 << endl;
+cout << "Integer4 (copied and assigned  from Integer2): " << Integer4 << endl;
+cout << "Integer5 (using the constructor with arguments): " << Integer5 << endl;
+cout << "Integer2 + Integer5 = " << Integer2+Integer5 << endl;
+cout << "Integer5 - Integer2 = " << Integer5-Integer2 << endl;
+cout << "Integer2 * Integer5 = " << Integer2*Integer5 << endl;
+cout << "Integer5 / Integer2 = " << Integer5/Integer2 << endl;
 }
 catch (exception& e) {
 cerr << "exception: " << e.what() << endl;
